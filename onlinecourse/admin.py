dc00b1a58ec32aac19450b69fa7ce1a4c7211319
Dklotz23 <devinmklotz@gmail.com>
2026-02-12 16:41:37 -0500

    Updated course_detail_bootstrap.html; added auth and questions

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -15,7 +15,6 @@ class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
-
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
     inlines = [LessonInline]
@@ -23,10 +22,12 @@ class CourseAdmin(admin.ModelAdmin):
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
 
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['content']
 
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
-
 
 # <HINT> Register Question and Choice models here
 admin.site.register(Question, QuestionAdmin)
